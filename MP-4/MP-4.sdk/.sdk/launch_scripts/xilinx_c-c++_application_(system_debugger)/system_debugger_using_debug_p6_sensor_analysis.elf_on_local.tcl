c5eca207e03bbea361e3f9f284d5bd0d77c1b18a
Joyce <ejoyce@iastate.edu>
2018-04-07 13:18:50 -0500

    all i do is win

---
+++ MP-4/MP-4.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_p6_sensor_analysis.elf_on_local.tcl
@@ -0,0 +1,11 @@
+connect -url tcp:127.0.0.1:3121
+source C:/Cpre488/MP-4/MP-4.sdk/MP4_design1_wrapper_hw_platform_2/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Platform Cable USB 00000000000000"} -index 0
+stop
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Platform Cable USB 00000000000000"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Platform Cable USB 00000000000000"} -index 0
+dow C:/Cpre488/MP-4/MP-4.sdk/p6_sensor_analysis/Debug/p6_sensor_analysis.elf
+bpadd -addr &main
